38b94604964167f93dc3a948b057366beb8dd7eb
Saunders <sbdhayes@gmail.com>
2015-06-29 12:50:18 -0700

    tuned functionality for youtube_replay.py - now works for simple youtube urls

--- youtube_replay.py
+++ youtube_replay.py
@@ -10,27 +10,37 @@
 
 # USAGE: python youtube_replay.py youtube_url
 
+import sys
+import os
+import re
+
 def main():
 	youtube_url = sys.argv[1]
 	match_object = re.search("\?v=(.+)", youtube_url)
 	video_id = ""
   	if not match_object:
+  		match_object = re.search("/embed/([_a-zA-Z0-9\-]+)", youtube_url)
+	if not match_object:
   		print sys.argv[1] + " is not a valid youtube url" 
   		sys.exit()
   	else:
   		video_id = match_object.group(1)
-  		print video_id 
+  		print
+  		print "Running mm-youtubereplay on video id " + video_id + "......"
+  		print 
 	media_files_path =  os.path.dirname(os.path.realpath(__file__)) + "/media_files/" + video_id
 	saved_requests_path = os.path.dirname(os.path.realpath(__file__)) + "/saved_requests/" + video_id
 	if not os.path.exists(saved_requests_path): 
-		print "ERROR: It appears you do not have any saved request response pairs for video id " + video_id
-		print "These requests could not be found in " + saved_requests_path
-		print "Please run python youtube_record.py " + youtube_url
+		print "ERROR: It appears there is no saved session data for the youtube video corresponding to video id " + video_id
+		print "ERROR: Recorded session data could not be found where it is expected in " + saved_requests_path
+		print "ERROR: Please run python youtube_record.py " + youtube_url + " to correct this error "
+		print
 		sys.exit()
 	if not os.path.exists(media_files_path):
-		print "ERROR: It appears you have downloaded the youtube video with video id " + video_id
-		print "The downloaded media files could not be found in " + media_files_path
-		print "Please run python youtube_record.py " + youtube_url
+		print "ERROR: It appears there are no saved media files for the youtube video corresponding to video id " + video_id
+		print "ERROR: Downloaded media files could not be found where they are expected in " + media_files_path
+		print "ERROR: Please run python youtube_record.py " + youtube_url + " to correct this error "
+		print
 		sys.exit()
 	os.system("mm-youtubereplay " + saved_requests_path + " chromium-browser --ignore-certificate-errors --user-data-dir=/tmp/nonexistent$(date +%s%N) " + youtube_url)
 		
