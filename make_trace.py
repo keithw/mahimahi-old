37da642577e1585cfb0c5823bba64a191443eac8
Saunders <sbdhayes@gmail.com>
2015-06-30 14:27:12 -0700

    added synthetic stall trace example and updated trace making skeleton program

--- make_trace.py
+++ make_trace.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+import os
 
 def main():
+	if(os.path.exists("./synthetic_traces/stall_example.down")):
+		os.system("rm ./synthetic_traces/stall_example.down")
 	f = file('./synthetic_traces/stall_example.down', 'w')
-	for x in range(0, 3000):
+	for x in range(0, 4000):
 		print(str(x), file=f)
-	for x in range(3000, 10000, 50):
+	for x in range(4000, 20050, 50):
 		print(str(x), file=f)
-
+	for x in range(20050, 100000):
+		print(str(x), file=f)
 
 if __name__ == '__main__':
   main()
