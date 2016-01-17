09d85b7e9af1597b477d272bcd7031785ad597c8
Greg D. Hill <greghill@cs.stanford.edu>
2016-01-16 21:28:00 -0800

    new parse inter frame delay implementation

--- new_analysis_scripts/parse-inter-frame-delay.py
+++ new_analysis_scripts/parse-inter-frame-delay.py
@@ -8,29 +8,39 @@ import collections
 import pprint
 import math
 from decimal import *
+getcontext().prec = 6 #the precision of our syscall
 
 # map takes only one argument so use a three-tuple as input
 def get_inter_frame_delay((stall_logfilename, trial_id, output_directory)):
-    with open(output_directory + "/" + trial_id + "/" + "inter-frame-delays.dat", 'w') as inter_frame_delay_file:
-        with open(output_directory + "/" + trial_id + "/" + "resume-delays.dat", 'w') as resumes_delay_file:
-            with open(stall_logfilename) as stall_logfile:
-                previous_render_call_time = Decimal(0.0)
-                previous_time_in_video = 0.0
-                for line in stall_logfile:
-                    match_object = re.search("RENDER CALL ON: ([0-9]+(?:\.[0-9]+)?)s TIME: (.+)", line)
-                    if match_object:
-                        render_call_time = Decimal(match_object.group(2)) # TEMP make it wrong one
-                        time_in_video = float(match_object.group(1))
-                        if previous_render_call_time != Decimal(0.0):
-                            print(render_call_time - previous_render_call_time, file=inter_frame_delay_file)
+    inter_frame_delay_filename = output_directory + "/" + trial_id + "/" + "inter-frame-delays.dat"
+    resumes_delay_filename = output_directory + "/" + trial_id + "/" + "resume-delays.dat"
+    with open(inter_frame_delay_filename, 'w') as inter_frame_delay_file, open(resumes_delay_filename, 'w') as resumes_delay_file, open(stall_logfilename) as stall_logfile:
 
-                        # Assume seek if move more than 1s in video
-                        if previous_time_in_video != 0.0 and time_in_video - previous_time_in_video > 1:
-                            print(render_call_time - previous_render_call_time, file=resumes_delay_file)
+        first_render_time_of_last_frame = Decimal(0.0)
+        time_in_video_of_last_frame = float(0)
 
-                        previous_render_call_time = render_call_time
-                        previous_time_in_video = time_in_video
-                print("Finished parsing " + trial_id)
+        for line in stall_logfile:
+            match_object = re.search("RENDER CALL ON: ([0-9]+(?:\.[0-9]+)?)s TIME: (.+)", line)
+            if match_object:
+                if first_render_time_of_last_frame == 0: # very first render call
+                    first_render_time_of_last_frame = Decimal(match_object.group(2))
+                    time_in_video_of_last_frame = float(match_object.group(1))
+                else: # every other render call
+                    system_time_of_render_call = Decimal(match_object.group(2))
+                    time_in_video_of_frame_rendered = float(match_object.group(1))
+
+                    if time_in_video_of_frame_rendered != time_in_video_of_last_frame: # We are rendering a new frame
+                        inter_frame_delay_time = system_time_of_render_call - first_render_time_of_last_frame - (Decimal(1) / Decimal(60))
+                        print(inter_frame_delay_time, file=inter_frame_delay_file)
+
+                        if (time_in_video_of_frame_rendered - time_in_video_of_last_frame) > 1: # assume seek if video jumps >1 s
+                            print(inter_frame_delay_time, file=resumes_delay_file)
+
+                        first_render_time_of_last_frame = system_time_of_render_call
+                        time_in_video_of_last_frame = time_in_video_of_frame_rendered
+
+
+        print("Finished parsing " + trial_id)
  
 def main():
     if len( sys.argv ) is not 3:
